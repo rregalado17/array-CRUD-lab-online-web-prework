8a6bf6e9b23366f8efdc03fd1782d6362cbf4197
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 18:52:56 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element_to_end_of_array("learning")
+  add_element_to_end_of_array.push("learning")
 end
 
 def add_element_to_start_of_array(array, element)
