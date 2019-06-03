48508fd4b78ec03475618c9e229fa21749c3c8ba
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 18:51:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  another_array =[a, b, c, d]
+  another_array =["wow", "i", "am", "really"]
 end
 
 def add_element_to_end_of_array(array, element)
